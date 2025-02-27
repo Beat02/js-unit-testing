9c342bf7257e9381cff01cbe97a97f35245c4aca
Beatriz R. Aguiar <beatriz.ruiz7@educa.madrid.org>
2025-02-27 19:54:04 +0100

    Initial commit

---
+++ feature-test-1/user-visits-root-tets.js
@@ -0,0 +1,10 @@
+const { assert } = require('chai');
+
+/* We want to make sure that when there are no poems in the database, there are no poems rendered on the homepage. This is the application’s empty state
+ */
+describe('User visits root', () => {
+    // ‘root’, refers to our application’s entry point
+    describe('without existing messages', () => {
+
+    });
+});
